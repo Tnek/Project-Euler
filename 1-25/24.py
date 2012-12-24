2d2c710e2b94816f297f2064adb99244bf35b481
tnek <Kent0526@gmail.com>
2012-12-24 18:01:19 -0500

    foo

--- 1-25/24.py
+++ 1-25/24.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 import itertools
-for i in [i for i in itertools.permutations('0123456789', 10)][1000000]: print(i, end="")
+print("".join([i for i in itertools.permutations('0123456789',10)][999999]))
