831e6bff4afe9236a1b1989efb8a302fc2352162
tnek <Kent0526@gmail.com>
2012-12-25 09:59:05 -0500

    Found small way of slightly improving run time

--- 26-50/39.py
+++ 26-50/39.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 b = lambda a, p: (p**2 - 2*p*a) / (2*p - 2*a)
-check = lambda p: len([True for a in range(p) if b(a, p) == int(b(a, p))])
-
+f = lambda p: len([True for a in range(int(p/2)) if b(a, p) == int(b(a, p))])
 temp = [0, 0]
 for p in range(1000):
-    if temp[0] < check(p):
-        temp[0] = check(p)
-        temp[1] = p
-print(temp)
+    if temp[0] < f(p):
+        temp[0] = f(p); temp[1] = p
+print(temp[1])
