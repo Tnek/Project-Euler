916fbb98f5f49aead1aacc93006eb777d33ec810
tnek <Kent0526@gmail.com>
2012-12-24 21:41:09 -0500

    Resolved with Sieve of Eratosthenes

--- 1-25/07.py
+++ 1-25/07.py
@@ -1,13 +1,7 @@
-#!/usr/bin/python
 import math
-def prime_check(x):
-    if x == 2: return True
-    if x % 2 == 0: return False
-    return not any(x % i == 0 for i in range(2, int(math.sqrt(x))+2))
-count = 0 ; x = 2 ; plist = []
-while count <= 10001:
-    if prime_check(x):
-        plist.append(x)
-        count += 1
-    x += 1
-print(plist[10000])
+n = 200000
+plist = [False] * 2 + [True] * n
+for i in range(2, int(math.sqrt(n))):
+    if plist[i]:
+        for j in range(i**2, n, i): plist[j] = False
+print([i for i in range(len(plist)) if plist[i]][10000])
