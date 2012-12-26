4daf49e02e2244c89a56b75392ae3602f39e0b54
tnek <Kent0526@gmail.com>
2012-12-25 20:49:15 -0500

    Sieve of eratosthenes

--- Python/1-25/10.py
+++ Python/1-25/10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import math
-def check(x):
-    if x == 2:  return True
-    if x < 2 or x % 2 == 0:  return False
-    return not any(x % i == 0 for i in range(3, int(math.sqrt(x)) + 1, 2))
-print(sum([i for i in range(2, 2000000) if check(i)]))
+n = 2000000; pl = [False] * 2 + [True] * (n-1)
+for i in range(2, int(math.sqrt(n))+2):
+    if pl[i]:
+        for j in range(i**2, n+1, i): pl[j] = False
+print(sum([i for i in range(len(pl)) if pl[i]]))
