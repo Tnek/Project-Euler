4c87962ab96a1e66f17b335e32a3a772e9c3dc0c
tnek <Kent0526@gmail.com>
2012-12-25 19:32:02 -0500

    mesed up implementing sieve of eratosthenes

--- Python/1-25/07.py
+++ Python/1-25/07.py
@@ -1,7 +1,7 @@
 import math
 n = 200000
 plist = [False] * 2 + [True] * n
-for i in range(2, int(math.sqrt(n))):
+for i in range(2, int(math.sqrt(n))+2):
     if plist[i]:
-        for j in range(i**2, n, i): plist[j] = False
+        for j in range(i**2, n+1, i): plist[j] = False
 print([i for i in range(len(plist)) if plist[i]][10000])
