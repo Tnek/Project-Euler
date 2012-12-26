ef3a392a8509e106e55a5f777ded8430d130fe99
tnek <Kent0526@gmail.com>
2012-12-25 20:34:16 -0500

    one lined

--- Python/51-75/56.py
+++ Python/51-75/56.py
@@ -1,12 +1,2 @@
 #!/usr/bin/python
-s = 0
-ans = 0
-for a in range(100):
-    for b in range(100):
-        c = str(a**b)
-        for x in range(len(c)):
-            s += int(c[x])
-        if s > ans:
-            ans = s
-        s = 0
-print(ans)
+print(max([sum([int(i) for i in c]) for c in [str(a**b) for a in range(100) for b in range(100)]]))
