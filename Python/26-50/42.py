b117f2d9a2a05d8294b9f8706bd1ce6e222c23e0
tnek <Kent0526@gmail.com>
2012-12-25 20:22:48 -0500

    foo

--- Python/26-50/42.py
+++ Python/26-50/42.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 import math
-FILE = open("42.txt", "r")
-temp = FILE.read()
-wlist = [word.strip('"') for word in temp.split(',')] 
+FILE = open("42.txt", "r").read()
+word = [i.strip('"') for i in FILE.split(',')] 
 
 letters = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-def score(x): return sum([letters.index(x[i]) for i in range(len(x))])
+score = lambda x: sum([letters.index(x[i]) for i in range(len(x))])
 
 def tri(x):
     y = (math.sqrt(8*x + 1) - 1) / 2
     return y == int(y)
-print(len([i for i in wlist if tri(score(i))]))
+print(len([i for i in word if tri(score(i))]))
