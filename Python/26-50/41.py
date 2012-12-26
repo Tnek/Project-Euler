8246aad4be5ad7b686cfbf2746e8f4e1c0373c23
tnek <Kent0526@gmail.com>
2012-12-25 20:20:45 -0500

    LAMBDAS

--- Python/26-50/41.py
+++ Python/26-50/41.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import math
-def pan_check(x): return sorted(list(str(x))) == list("123456789"[:len(str(x))])
-def pcheck(x): return not any(x % i == 0 for i in range(3, int(math.sqrt(x)) + 2, 2))
+pan = lambda x: sorted(list(x)) == list("123456789"[:len(x)])
+prime = lambda x: not any(x%i==0 for i in range(3, int(math.sqrt(x))+2,2))
 x = 7654321
-while not pan_check(x) or not pcheck(x): x -= 2
+while not pan(str(x)) or not prime(x): x -= 2
 print(x)
