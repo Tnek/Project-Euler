19a2d257fcba02130e798cbd07cd127d13c57194
tnek <Kent0526@gmail.com>
2012-12-25 20:23:34 -0500

    foo

--- Python/26-50/45.py
+++ Python/26-50/45.py
@@ -1,8 +1,8 @@
 import math
 hexa = lambda x: x * (2*x - 1)
-def penta_check(x):
+def pent(x):
     y = (math.sqrt(1 + 24*x) + 1) / 6
     return y == int(y)
 x = 144
-while not penta_check(hexa(x)): x += 1
+while not pent(hexa(x)): x += 1
 print(hexa(x))
