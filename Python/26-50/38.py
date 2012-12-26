0e3c5688d3d5ede655ab65572dadb581f0ef5c47
tnek <Kent0526@gmail.com>
2012-12-25 20:16:29 -0500

    slightly shortened

--- Python/26-50/38.py
+++ Python/26-50/38.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
-def pan_check(x): return sorted(list(str(x))) == list("123456789")
-print(max([str(x) + str(x*2) for x in range(9876) if pan_check(int(str(x) + str(x*2)))])) 
+def pan(x): return sorted(list(str(x))) == list("123456789")
+print(max([str(x) + str(x*2) for x in range(9876) if pan(int(str(x) + str(x*2)))])) 
