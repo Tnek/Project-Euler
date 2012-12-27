98e2407adf97690b56510b20e9dba8f0ccae40b4
tnek <Kent0526@gmail.com>
2012-12-27 11:43:34 -0500

    Shortened further

--- Python/1-25/03.py
+++ Python/1-25/03.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 import math
-print(max([i for i in range(3, int(math.sqrt(600851475143)) + 2, 2) if 600851475143 % i == 0 and not any(i % j == 0 for j in range(3, int(math.sqrt(i))+2, 2))]))
+print(max(i for i in range(3, int(math.sqrt(600851475143)) + 2, 2) if 600851475143 % i == 0 and not any(i % j == 0 for j in range(3, int(math.sqrt(i))+2, 2))))
--- Python/1-25/06.py
+++ Python/1-25/06.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python
-print(sum([i*i for i in range(101)]) - sum([i for i in range(101)])**2)
+print(sum(i*i for i in range(101)) - sum(i for i in range(101))**2)
