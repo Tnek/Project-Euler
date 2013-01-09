1ad6d27491a992f00a6be1c6b57a2c30fc7251ee
tnek <Kent0526@gmail.com>
2013-01-09 18:35:59 -0500

    Shortened

--- Python/1-25/14.py
+++ Python/1-25/14.py
@@ -2,19 +2,12 @@
 def chain(x):
     count = 1
     while x != 1:
-        if x % 2 == 0:
-            x /= 2
-            count += 1
-        else:
-            x = (x * 3) + 1
-            count += 1
+        if x % 2 == 0: x /= 2
+        else: x = (x * 3) + 1
+        count += 1
     return count
 
-count = 500001
 temp = 0
-while count < 1000000:
-    if chain(count) > temp:
-        ans = count
-        temp = chain(count)
-    count += 2
-print(ans)
+n = [i for i in range(500001, 1000000, 2)]
+y = [chain(i) for i in range(500001, 1000000, 2)]
+print(n[y.index(max(y))])
