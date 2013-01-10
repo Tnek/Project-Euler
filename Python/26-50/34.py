a7fc5ab8ec5e2894f7de46f2ed2b29c74a8ba9d3
tnek <Kent0526@gmail.com>
2013-01-09 19:03:31 -0500

    n

--- Python/26-50/34.py
+++ Python/26-50/34.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-l = [ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 ]
+n = [ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 ]
 temp = count = 0
 x = 146
 ans = 145
 while True:
     for i in range(len(str(x))):
-        temp += l[int(str(x)[i])]
+        temp += n[int(str(x)[i])]
     if temp == x:
         ans += x
         print(ans)
