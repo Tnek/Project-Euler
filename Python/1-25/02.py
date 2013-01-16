eb3ffcff0b30ce151f93258da115fda974399fd3
tnek <Kent0526@gmail.com>
2013-01-15 20:43:08 -0500

    shorter~

--- Python/1-25/02.py
+++ Python/1-25/02.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 from math import sqrt
-print(round(((((1+sqrt(5))/2)**35+((1-sqrt(5))/2)**35)/sqrt(5)-1)/2))
+print(int(((((1+sqrt(5))/2)**35+((1-sqrt(5))/2)**35)/sqrt(5)-1)/2+0.5))
