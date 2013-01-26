8c570cbfbbfe8b9ad8e6edf0cd46bcb76aa5ab27
tnek <tnekent@gmail.com>
2013-01-25 19:34:57 -0500

    SO UGLY

--- Python/76-100/80.py
+++ Python/76-100/80.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 from decimal import *
 getcontext().prec = 102
-print(sum(sum(int(j)for j in"".join(str(Decimal(i).sqrt()).split("."))[:100])for i in range(2,100)if not i in{i**2 for i in range(2,10)}))
-
+print(sum(sum(int(j)for j in str(int(Decimal(i).sqrt()*10**99)))for i in range(2,100)if not i in{i**2 for i in range(2,10)}))
