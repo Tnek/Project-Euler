e553c7c905b8b0c78f7aa40125c34a9872e5856c
tnek <tnekent@gmail.com>
2013-01-25 19:36:57 -0500

    further codegolfing

--- Python/76-100/80.py
+++ Python/76-100/80.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
-from decimal import *
-getcontext().prec = 102
+from decimal import*
+getcontext().prec=102
 print(sum(sum(int(j)for j in str(int(Decimal(i).sqrt()*10**99)))for i in range(2,100)if not i in{i**2 for i in range(2,10)}))
